226e13dfdc15b8ffcb46a10a912cf250fce0125e
Justin Vittitoe <justinvittitoe7@gmail.com>
2025-04-27 19:16:02 -0600

    finished the thought and reaction routes

--- src/routes/api/thoughtsRouter.ts
+++ src/routes/api/thoughtsRouter.ts
@@ -2,12 +2,25 @@ import { Router } from 'express';
 const router = Router();
 import {
     getAllThoughts,
-    getThoughtById
+    getThoughtById,
+    createNewThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
 } from '../../controllers/thoughtsController.js';
 
-router.route('/').get(getAllThoughts);
+router.route('/').get(getAllThoughts).post(createNewThought);
 
 router.route('/:thoughtId')
 .get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought)
+
+router.route('/:thoughtId/reaction')
+.post(createReaction)
+
+router.route('/:thoughtId/reaction/:reactionId')
+.delete(deleteReaction)
 
 export { router as thoughtsRouter };
